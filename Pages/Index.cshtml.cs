be6ffccd43344875db57524ae01f8bf0eb098431
Gubbiii <lucasrm@live.dk>
2024-10-01 10:34:16 +0200

    opdatering med json

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,3 +1,6 @@
+using HandleLet.Models;
+using HandleLet.Services;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -6,15 +9,21 @@ namespace HandleLet.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private RecipeServices recipeService;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, RecipeServices recipeServices)
         {
             _logger = logger;
+            this.recipeService = recipeServices;
         }
+
 
         public void OnGet()
         {
-
+            recipes = recipeService.GetUserStories();
         }
+        public List<Recipe> recipes { get; private set; }
+
+        
     }
 }
