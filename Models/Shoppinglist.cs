d2accf5f8c3a9d205d18fbb16619f12ecb7a70cd
JaisAndersen <jais.h.sorensen@gmail.com>
2024-10-01 11:26:42 +0200

    Shopping List Model

--- Models/Shoppinglist.cs
+++ Models/Shoppinglist.cs
@@ -1,6 +1,56 @@
-﻿namespace HandleLet.Models
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace HandleLet.Models
 {
-    public class Shoppinglist
+    public class ShoppingList
     {
+        private static int _nextId = 0;
+        private int _id;
+        private string _name;
+        private Ingredients _ingredients;
+
+        void NextId()
+        {
+            _nextId++;
+            _id = _nextId;
+        }
+
+        [BindProperty]
+        [Display(Name = "Id")]
+        [Range(minimum: 0, maximum: 1000)]
+        public int Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        [Display(Name = "Navn")]
+        [Required(ErrorMessage = "ShoppingList skal have et navn")]
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public Ingredients Ingredients
+        {
+            get { return _ingredients; }
+            set { _ingredients = value; }
+        }
+
+        public ShoppingList()
+        {
+            _id = 0;
+            _name = "";
+            _ingredients = null;
+        }
+
+        public ShoppingList(string name, Ingredients ingredients)
+        {
+            NextId();
+            _name = name;
+            _ingredients = ingredients;
+        }
     }
 }
