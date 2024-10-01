1adbcc217462f347762b07f19799abe6221780f4
JaisAndersen <jais.h.sorensen@gmail.com>
2024-10-01 10:01:54 +0200

    Models

--- Models/Ingredients.cs
+++ Models/Ingredients.cs
@@ -2,5 +2,14 @@
 {
     public class Ingredients
     {
+        private static int _nextId = 0;
+        private int _id;
+        private string _name;
+
+        void NextId()
+        { 
+            _nextId++;
+            _id = _nextId;
+        }
     }
 }
