371da6c14b18b4b9d4bc3a9bce90a7d0b48d2c96
Gubbiii <lucasrm@live.dk>
2024-10-01 09:57:59 +0200

    recipe

---
+++ Models/Recipe.cs
@@ -0,0 +1,17 @@
+﻿namespace HandleLet.Models
+{
+    public class Recipe
+    {
+
+
+        public Recipe(string Title, string Description, int BusinessValue, int Priority, string StoryPoints, DateTime CreationDate)
+        {
+            this.Title = Title;
+            this.Description = Description;
+            
+            
+
+            Id = NextId++;
+        }
+    }
+}
---
+++ Pages/CRUDRecipe/ReadRecipe.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HandleLet.Pages.CRUDRecipe
+{
+    public class ReadRecipeModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
