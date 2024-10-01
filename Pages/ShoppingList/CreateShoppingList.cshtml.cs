c958fdafb0322e3dd588068a7c40b1758e63ffac
JaisAndersen <jais.h.sorensen@gmail.com>
2024-10-01 11:50:04 +0200

    Pages added

---
+++ Pages/ShoppingList/CreateShoppingList.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HandleLet.Pages.ShoppingList
+{
+    public class CreateShoppingListModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Pages/ShoppingList/DeleteShoppingList.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HandleLet.Pages.ShoppingList
+{
+    public class DeleteShoppingListModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
