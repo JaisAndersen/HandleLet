40bc575578b815f949321dfe0cf2136ce91f624a
Gubbiii <lucasrm@live.dk>
2024-10-01 11:15:33 +0200

    Json opdatering

--- JsonHelpers/JsonFileReaderRecipe.cs
+++ JsonHelpers/JsonFileReaderRecipe.cs
@@ -1,6 +1,16 @@
-﻿namespace HandleLet.JsonHelpers
+﻿using HandleLet.Models;
+using Newtonsoft.Json;
+
+namespace HandleLet.JsonHelpers
 {
     public class JsonFileReaderRecipe
     {
+
+        public static List<Recipe> ReadJson(string JsonFileName)
+        {
+            string jsonString = File.ReadAllText(JsonFileName);
+            using (var jsonFileReader = File.OpenText(JsonFileName))
+            { return JsonConvert.DeserializeObject<List<Recipe>>(jsonString); }
+        }
     }
 }
