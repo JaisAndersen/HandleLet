cceaa933682b96d4cb5d8bb824d857aae4e50545
JaisAndersen <jais.h.sorensen@gmail.com>
2024-10-01 10:12:56 +0200

    Ingredients model opdateret

--- Models/Ingredients.cs
+++ Models/Ingredients.cs
@@ -1,4 +1,7 @@
-﻿namespace HandleLet.Models
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace HandleLet.Models
 {
     public class Ingredients
     {
@@ -10,6 +13,36 @@
         { 
             _nextId++;
             _id = _nextId;
+        }
+        
+        [BindProperty]
+        [Display(Name = "Id")]       
+        [Range(minimum: 0, maximum: 1000)]
+        public int Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+        
+        [Display(Name = "Navn")]
+        [Required(ErrorMessage = "Ingrediensen skal have et navn")]
+        
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+        
+        public Ingredients()
+        {
+            _id = 0;
+            _name = "";
+        }
+
+        public Ingredients(string name)
+        {
+            NextId();
+            name = _name;            
         }
     }
 }
